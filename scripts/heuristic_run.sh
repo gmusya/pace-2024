66c1731622b376d696004a064763cac11ce88e19
German Perov <ggmusya@gmail.com>
2024-04-30 15:08:13 +0300

    Fix script

--- scripts/heuristic_run.sh
+++ scripts/heuristic_run.sh
@@ -7,4 +7,4 @@ do
   time $HEURISTIC_EXECUTABLE < $EXAMPLES_DIR/${i}.gr > $OUTPUT_DIR/${i}.sol 2>$OUTPUT_DIR/${i}.log
 done
 
-cat $OUTPUT_DIR/*.log | grep final_score > $OUTPUT_DIR/result.log
+cat $OUTPUT_DIR/{1..100}.log | grep final_score > $OUTPUT_DIR/result.log
