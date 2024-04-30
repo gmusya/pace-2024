ab4bf4dc33d37cc5b820f7c355cbacb64e326bb6
German Perov <ggmusya@gmail.com>
2024-04-30 11:42:16 +0300

    Fix bug, use hungarian if possible

--- src/heuristic/greedy.cpp
+++ src/heuristic/greedy.cpp
@@ -4,8 +4,72 @@
 #include <cstdint>
 #include <fstream>
 #include <iostream>
+#include <limits>
 #include <numeric>
 #include <optional>
+
+namespace hungarian {
+
+// https://cp-algorithms.com/graph/hungarian-algorithm.html#implementation-of-the-hungarian-algorithm
+
+// minimization
+std::vector<int> Hungarian(const std::vector<std::vector<int64_t>>& weights) {
+  ENSURE_OR_THROW(!weights.empty() && weights.size() <= weights[0].size());
+
+  const int64_t kInfinity = [&]() {
+    int64_t result = std::numeric_limits<int64_t>::min();
+    for (const auto& row : weights) {
+      for (const auto& value : row) {
+        result = std::max(result, value);
+      }
+    }
+    return result;
+  }();
+
+  int n = static_cast<int>(weights.size()) - 1;
+  int m = static_cast<int>(weights[0].size()) - 1;
+  std::vector<int> u(n + 1), v(m + 1), p(m + 1), way(m + 1);
+  for (int i = 1; i <= n; ++i) {
+    p[0] = i;
+    int j0 = 0;
+    std::vector<int> minv(m + 1, kInfinity);
+    std::vector<bool> used(m + 1, false);
+    do {
+      used[j0] = true;
+      int i0 = p[j0], delta = kInfinity, j1;
+      for (int j = 1; j <= m; ++j)
+        if (!used[j]) {
+          int cur = weights[i0][j] - u[i0] - v[j];
+          if (cur < minv[j])
+            minv[j] = cur, way[j] = j0;
+          if (minv[j] < delta)
+            delta = minv[j], j1 = j;
+        }
+      for (int j = 0; j <= m; ++j)
+        if (used[j])
+          u[p[j]] += delta, v[j] -= delta;
+        else
+          minv[j] -= delta;
+      j0 = j1;
+    } while (p[j0] != 0);
+    do {
+      int j1 = way[j0];
+      p[j0] = p[j1];
+      j0 = j1;
+    } while (j0);
+  }
+
+  std::vector<int> ans(n);
+  for (int j = 1; j <= m; ++j) {
+    ans[p[j] - 1] = j - 1;
+  }
+  int64_t cost = -v[0];
+  std::cerr << "hungarian cost = " << cost << '\n';
+
+  return ans;
+}
+
+}// namespace hungarian
 
 namespace ocm {
 
@@ -14,7 +78,7 @@ namespace heuristic {
   constexpr uint64_t kMemoryLimitBytes = 8ull * 1024 * 1024 * 1024;
 
   namespace greedy {
-    using Weight = uint64_t;
+    using Weight = int64_t;
     using Matrix = std::vector<std::vector<Weight>>;
 
     Matrix BuildMatrix(const Task& task) {
@@ -34,7 +98,7 @@ namespace heuristic {
         Vertex left_from_me = 0;
         for (Vertex position = 0; position < task.b_size; ++position) {
           matrix[v][position] = current_weight;
-          if (left_from_me < neighbors[v].size() && left_from_me == neighbors[v].size()) {
+          if (left_from_me < neighbors[v].size() && position == neighbors[v][left_from_me]) {
             ++left_from_me;
           }
           current_weight += left_from_me;
@@ -48,6 +112,21 @@ namespace heuristic {
     Solution Solve(const Task& task) {
       auto matrix = BuildMatrix(task);
       // TODO: use hungarian algorithm
+
+      if (task.b_size < 3000) {
+        std::vector<std::vector<int64_t>> weights(task.b_size + 1,
+                                                  std::vector<int64_t>(task.b_size + 1));
+        for (Vertex v = 0; v < task.b_size; ++v) {
+          for (Vertex p = 0; p < task.b_size; ++p) {
+            weights[v + 1][p + 1] = matrix[v][p];
+          }
+        }
+
+        auto solution_hungraian = hungarian::Hungarian(weights);
+        Solution solution(solution_hungraian.begin(), solution_hungraian.end());
+
+        return solution;
+      }
 
       std::vector<bool> position_is_taken(task.b_size);
       Solution solution(task.b_size);
@@ -66,6 +145,7 @@ namespace heuristic {
           break;
         }
       }
+
       return solution;
     }
 
